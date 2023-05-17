aeaffa8f2e74e53fe0f3b0ba5712f297bbc5c6a1
Layane Nunes <layanenu@gmail.com>
2023-05-17 11:06:33 -0300

    feat: funcao fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,21 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)  # para a req aguardar 1 seg em cada req que fizer
+    try:
+        response = requests.get(
+            url, timeout=3, headers={"user-agent": "Fake user-agent"})
+        if response.status_code == 200:  # caso a req for ok retorna o text
+            return response.text
+    except requests.ReadTimeout:
+        return None
+
+
+# html = fetch("https://blog.betrybe.com/")
+# print(html)
 
 
 # Requisito 2
